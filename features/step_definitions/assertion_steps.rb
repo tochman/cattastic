99e282c95cdcbb47168dcacdcb190a426be59b2b
Zak <zakary.howard94@gmail.com>
2018-10-03 12:34:12 +0200

    removed out of scoped data

--- features/step_definitions/assertion_steps.rb
+++
@@ -1,3 +0,0 @@
-Then("I should see {string}") do |content|
-    expect(page).to have_content content
-end
--- features/step_definitions/basic_steps.rb
+++
@@ -1,3 +0,0 @@
-# Given("That I am on the landing page") do
-#     visit root_path  
-# end
